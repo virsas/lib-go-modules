dbd2ac1bb5b9a432743af20a5be5843761dcac35
Stefan Moravcik <stefan@virsas.com>
2023-07-11 09:57:01 +0200

    added debug for request body

--- vssutil/debug.go
+++ vssutil/debug.go
@@ -1,26 +1,56 @@
 package vssutil
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
 )
 
 func DebugClaims(claims jwt.MapClaims) {
-	var logClaims bool = false
-	logClaimsValue, logClaimsPresent := os.LookupEnv("CLAIMS_DEBUG")
-	if logClaimsPresent {
-		logClaimsValueBool, err := strconv.ParseBool(logClaimsValue)
+	var claimsDebug bool = false
+	claimsDebugValue, claimsDebugPresent := os.LookupEnv("CLAIMS_DEBUG")
+	if claimsDebugPresent {
+		claimsDebugValueBool, err := strconv.ParseBool(claimsDebugValue)
 		if err != nil {
-			logClaims = false
+			claimsDebug = false
 		}
-		logClaims = logClaimsValueBool
+		claimsDebug = claimsDebugValueBool
 	}
-	if logClaims {
+	if claimsDebug {
 		fmt.Println("######### Log Claims Function #########")
 		fmt.Println(claims)
 		fmt.Println("######### Log Claims Function #########")
+	}
+}
+
+func DebugRequestBody(c *gin.Context) {
+	var requestDebug bool = false
+	requestDebugValue, requestDebugPresent := os.LookupEnv("REQUEST_DEBUG")
+	if requestDebugPresent {
+		requestDebugValueBool, err := strconv.ParseBool(requestDebugValue)
+		if err != nil {
+			requestDebug = false
+		}
+		requestDebug = requestDebugValueBool
+	}
+	if requestDebug {
+		data, _ := io.ReadAll(c.Request.Body)
+		c.Request.Body.Close()
+		c.Request.Body = io.NopCloser(bytes.NewBuffer(data))
+
+		body := map[string]interface{}{
+			"Action":   c.Request.Method,
+			"Resource": c.Request.RequestURI,
+			"Body":     string(data),
+		}
+
+		fmt.Println("######### Log Request Body #########")
+		fmt.Println(body)
+		fmt.Println("######### Log Request Body #########")
 	}
 }
