603401f706dda50863eecd40186ffe734b423307
Stefan Moravcik <stefan@virsas.com>
2023-09-27 15:54:13 +0200

    added alphaspace validator

--- vssutil/customValidator.go
+++ vssutil/customValidator.go
@@ -10,11 +10,13 @@ const nonEnglishChars string = "àáâäæãåāăąçćčđďèéêëēėęěğ
 const (
 	AlphaNumSpecialString = `^[a-zA-Z0-9\s` + nonEnglishChars + `!@#$_%^&*.,?()-=+:;|'<>-]+$`
 	SimpleSentenceString  = `^[a-zA-Z0-9\s` + nonEnglishChars + `\.\,\!]+$`
+	AlphaSpaceString      = `^[a-zA-Z0-9\s` + nonEnglishChars + `]+$`
 )
 
 var (
 	AlphaNumSpecialRegex = regexp.MustCompile(AlphaNumSpecialString)
 	SimpleSentenceRegex  = regexp.MustCompile(SimpleSentenceString)
+	AlphaSpaceRegex      = regexp.MustCompile(AlphaSpaceString)
 )
 
 func AlphaNumSpecialValid(fl validator.FieldLevel) bool {
@@ -22,4 +24,7 @@ func AlphaNumSpecialValid(fl validator.FieldLevel) bool {
 }
 func SimpleSentenceValid(fl validator.FieldLevel) bool {
 	return SimpleSentenceRegex.MatchString(fl.Field().String())
+}
+func AlphaSpaceValid(fl validator.FieldLevel) bool {
+	return AlphaSpaceRegex.MatchString(fl.Field().String())
 }
