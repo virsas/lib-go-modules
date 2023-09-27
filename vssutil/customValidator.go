80dde3b86e88b851d16916850f166da430972e00
Stefan Moravcik <stefan@virsas.com>
2023-09-27 13:12:10 +0200

    added simple sentence validation

--- vssutil/customValidator.go
+++ vssutil/customValidator.go
@@ -9,12 +9,17 @@ import (
 const nonEnglishChars string = "àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłľḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźżÀÁÂÄÆÃÅĀĂĄÇĆČĐĎÈÉÊËĒĖĘĚĞǴḦÎÏÍĪĮÌŁĽḾÑŃǸŇÔÖÒÓŒØŌÕŐṔŔŘßŚŠŞȘŤȚÛÜÙÚŪǗŮŰŲẂẌŸÝŽŹŻ"
 const (
 	AlphaNumSpecialString = `^[a-zA-Z0-9\s` + nonEnglishChars + `!@#$_%^&*.,?()-=+:;|'<>-]+$`
+	SimpleSentenceString  = `^[a-zA-Z0-9\s` + nonEnglishChars + `\.\,\!]+$`
 )
 
 var (
 	AlphaNumSpecialRegex = regexp.MustCompile(AlphaNumSpecialString)
+	SimpleSentenceRegex  = regexp.MustCompile(SimpleSentenceString)
 )
 
 func AlphaNumSpecialValid(fl validator.FieldLevel) bool {
 	return AlphaNumSpecialRegex.MatchString(fl.Field().String())
+}
+func SimpleSentenceValid(fl validator.FieldLevel) bool {
+	return SimpleSentenceRegex.MatchString(fl.Field().String())
 }
