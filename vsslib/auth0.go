9473767939675ff27f6aa7c9d5f96e6e58491137
Stefan Moravcik <stefan@virsas.com>
2023-09-27 14:06:52 +0200

    joined auth0 handler

--- vsslib/auth0.go
+++ vsslib/auth0.go
@@ -7,35 +7,43 @@ import (
 	"gopkg.in/auth0.v5/management"
 )
 
-type Auth0RoleHandler interface {
-	List() ([]Role, error)
-	Show(id string) (Role, error)
-	Create(name string, description string) error
-	Update(id string, name string, description string) error
-	Delete(id string) error
+type Auth0Handler interface {
+	RoleList() ([]Auth0Role, error)
+	RoleShow(id string) (Auth0Role, error)
+	RoleCreate(name string, description string) error
+	RoleUpdate(id string, name string, description string) error
+	RoleDelete(id string) error
 }
 
-type auth0role struct {
+type auth0Struct struct {
 	session *management.Management
 }
 
-type Role struct {
+type Auth0Role struct {
 	ID          string             `json:"id"`
 	Name        string             `json:"name"`
 	Description string             `json:"description"`
 	AuthUsers   []*management.User `json:"authUsers"`
 }
 
-func NewAuth0RoleSess(sess *management.Management) Auth0RoleHandler {
-	a := &auth0role{session: sess}
+func NewAuth0Session(domain string, client string, secret string) (Auth0Handler, error) {
+	var err error
+	var session *management.Management
 
-	return a
+	session, err = management.New(domain, management.WithClientCredentials(client, secret))
+	if err != nil {
+		return nil, err
+	}
+
+	a := &auth0Struct{session: session}
+
+	return a, nil
 }
 
-func (a *auth0role) List() ([]Role, error) {
+func (a *auth0Struct) RoleList() ([]Auth0Role, error) {
 	var err error
-	var role Role
-	var roles []Role = []Role{}
+	var role Auth0Role
+	var roles []Auth0Role = []Auth0Role{}
 
 	var page int = 0
 	for {
@@ -63,9 +71,9 @@ func (a *auth0role) List() ([]Role, error) {
 	return roles, err
 }
 
-func (a *auth0role) Show(id string) (Role, error) {
+func (a *auth0Struct) RoleShow(id string) (Auth0Role, error) {
 	var err error
-	var role Role
+	var role Auth0Role
 
 	authRole, err := a.session.Role.Read(id)
 	if err != nil {
@@ -85,7 +93,7 @@ func (a *auth0role) Show(id string) (Role, error) {
 	return role, nil
 }
 
-func (a *auth0role) Create(name string, description string) error {
+func (a *auth0Struct) RoleCreate(name string, description string) error {
 	var err error
 
 	r := &management.Role{
@@ -101,7 +109,7 @@ func (a *auth0role) Create(name string, description string) error {
 	return nil
 }
 
-func (a *auth0role) Update(id string, name string, description string) error {
+func (a *auth0Struct) RoleUpdate(id string, name string, description string) error {
 	var err error
 
 	r := &management.Role{
@@ -117,7 +125,7 @@ func (a *auth0role) Update(id string, name string, description string) error {
 	return nil
 }
 
-func (a *auth0role) Delete(id string) error {
+func (a *auth0Struct) RoleDelete(id string) error {
 	var err error
 
 	users, err := a.session.Role.Users(id)
