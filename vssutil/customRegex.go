ad9d458419e635b3a6e216abf40e9949e7738e8d
Stefan Moravcik <stefan@virsas.com>
2023-10-06 12:35:27 +0200

    added regexes for test output removal

--- vssutil/customRegex.go
+++ vssutil/customRegex.go
@@ -3,9 +3,13 @@ package vssutil
 import "regexp"
 
 const (
+	updatedString       = `,"createdAt":\s?"(20.*?)"`
+	createdString       = `,"updatedAt":\s?"(20.*?)"`
 	authorizationString = `"authorization":\s?"(.*?)"`
 )
 
 var (
+	UpdatedRegex       = regexp.MustCompile(updatedString)
+	CreatedRegex       = regexp.MustCompile(createdString)
 	AuthorizationRegex = regexp.MustCompile(authorizationString)
 )
