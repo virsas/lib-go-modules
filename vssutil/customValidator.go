25712e4d394120e5698084ea4be3b21a7a0b4b1a
Stefan Moravcik <stefan@virsas.com>
2023-11-02 13:31:21 +0100

    modified validators

--- vssutil/customValidator.go
+++ vssutil/customValidator.go
@@ -7,34 +7,36 @@ import (
 )
 
 const nonEnglishChars string = "àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłľḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźżÀÁÂÄÆÃÅĀĂĄÇĆČĐĎÈÉÊËĒĖĘĚĞǴḦÎÏÍĪĮÌŁĽḾÑŃǸŇÔÖÒÓŒØŌÕŐṔŔŘßŚŠŞȘŤȚÛÜÙÚŪǗŮŰŲẂẌŸÝŽŹŻ"
+const allSpecialChars string = `'"#@%&_!,:;~=<>\.\-\+\*\?\$\^\(\)\[\]\{\}\/\\\|`
+
 const (
-	AlphaNumSpecialString = `^[a-zA-Z0-9\s` + nonEnglishChars + `!@#$_%^&*.,?()-=+:;|'<>-]+$`
-	SimpleSentenceString  = `^[a-zA-Z0-9\s` + nonEnglishChars + `\.\,\!]+$`
-	AlphaSpaceString      = `^[a-zA-Z0-9\s` + nonEnglishChars + `]+$`
-	LowerDashString       = `^[a-z\-]+$`
-	AlphaNumDashString    = `^[a-zA-Z0-9\-]+$`
+	AlphaSpaceString      = `^[a-zA-Z` + nonEnglishChars + `\s]+$`
+	AlphaNumSpaceString   = `^[a-zA-Z0-9` + nonEnglishChars + `\s]+$`
+	AlphaNumSpecialString = `^[a-zA-Z0-9` + nonEnglishChars + allSpecialChars + `\s]+$`
+	SimpleSentenceString  = `^[\w` + nonEnglishChars + `\s'"\-\.?!]+$`
+	SlugString            = `^[a-z0-9]+(?:-[a-z0-9]+)*$`
 )
 
 var (
+	AlphaSpaceRegex      = regexp.MustCompile(AlphaSpaceString)
+	AlphaNumSpaceRegex   = regexp.MustCompile(AlphaNumSpaceString)
 	AlphaNumSpecialRegex = regexp.MustCompile(AlphaNumSpecialString)
 	SimpleSentenceRegex  = regexp.MustCompile(SimpleSentenceString)
-	AlphaSpaceRegex      = regexp.MustCompile(AlphaSpaceString)
-	LowerDashRegex       = regexp.MustCompile(LowerDashString)
-	AlphaNumDashRegex    = regexp.MustCompile(AlphaNumDashString)
+	SlugRegex            = regexp.MustCompile(SlugString)
 )
 
+func AlphaSpaceValid(fl validator.FieldLevel) bool {
+	return AlphaSpaceRegex.MatchString(fl.Field().String())
+}
+func AlphaNumSpaceValid(fl validator.FieldLevel) bool {
+	return AlphaNumSpaceRegex.MatchString(fl.Field().String())
+}
 func AlphaNumSpecialValid(fl validator.FieldLevel) bool {
 	return AlphaNumSpecialRegex.MatchString(fl.Field().String())
 }
 func SimpleSentenceValid(fl validator.FieldLevel) bool {
 	return SimpleSentenceRegex.MatchString(fl.Field().String())
 }
-func AlphaSpaceValid(fl validator.FieldLevel) bool {
-	return AlphaSpaceRegex.MatchString(fl.Field().String())
-}
-func LowerDashValid(fl validator.FieldLevel) bool {
-	return LowerDashRegex.MatchString(fl.Field().String())
-}
-func AlphaNumDashValid(fl validator.FieldLevel) bool {
-	return AlphaNumDashRegex.MatchString(fl.Field().String())
+func SlugValid(fl validator.FieldLevel) bool {
+	return SlugRegex.MatchString(fl.Field().String())
 }
