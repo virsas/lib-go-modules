a020ac06b1fe8b402aeb700fa33a5a946f645390
Stefan Moravcik <stefan@virsas.com>
2023-11-29 11:17:07 +0100

    fixed header for http request

--- vssutil/getExangeRate.go
+++ vssutil/getExangeRate.go
@@ -11,10 +11,20 @@ func GetExchangeRate(cur string) (float64, error) {
 	var err error
 	var rate string
 
-	resp, err := http.Get("http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt")
+	client := http.Client{}
+
+	req, err := http.NewRequest("GET", "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt", nil)
 	if err != nil {
 		return 0, err
 	}
+
+	req.Header.Add("Accept", `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`)
+	req.Header.Add("User-Agent", `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36`)
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+
 	defer resp.Body.Close()
 
 	scanner := bufio.NewScanner(resp.Body)
